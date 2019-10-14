bb578eb093a773c6ee51859cb1bb58956f863e30
William Maphanga <williesdk@gmail.com>
2019-10-14 18:08:26 +0200

    added get products endpoint NB needs to be fixed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import * as functions from "firebase-functions";
-
+import * as admin from "firebase-admin";
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((_, response) => {
-  console.log("Hello world");
-  response.send("Hello from Firebase! Again");
+export const getProducts = functions.https.onRequest((_, response) => {
+  const promise = admin
+    .firestore()
+    .collection("products")
+    .get();
+
+  promise
+    .then(snapshot => {
+      const data = snapshot;
+      response.send(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
